813f8a8d39862dea606f96662268e259cdc5ee5e
Jonno <jonnowelch@yahoo.com>
2020-03-29 11:56:36 +0100

    changed some text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <h1>Tasks to do</h1>
+      Thanks for checking out my task app. This is my first project using React
+      Context, Hooks and Local Storage.
       {tasks.length ? (
         <p>Items in task list: {tasks.length} </p>
       ) : (
